cd94f4817d75f40b3a695b8c46762e585a4b4ed6
farazkabbo <kabbofaraz@gmail.com>
2025-10-04 15:14:26 -0400

    I WANNA COMMIT

--- src/Learning.tsx
+++ src/Learning.tsx
@@ -649,7 +649,11 @@ const Learning = () => {
               
             </div>
             
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-4 ">
+              <button 
+              onClick={() => window.location.href = '/dashboard'}
+              className="px-4 py-3  bg-white/10 hover:bg-white/20 rounded-lg transition-colors text-white font-medium text-sm flex items-center gap-2 hover:cursor-pointer"
+            >Dashboard</button>
               <div className="flex items-center gap-2 bg-white/10 px-4 py-2 rounded-xl">
                 <Flame className="w-5 h-5 text-orange-400" />
                 <span className="text-white font-bold">{gameState.streak}</span>
